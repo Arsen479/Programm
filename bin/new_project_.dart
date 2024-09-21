0b3cbd2db495bb950954cb0558eedb4f67116324
Arsen <azybaliev91@gmail.com>
2024-09-21 18:46:38 +0300

    first commit

---
+++ bin/new_project_.dart
@@ -0,0 +1,5 @@
+import 'package:new_project_/new_project_.dart' as new_project_;
+
+void main(List<String> arguments) {
+  print('Hello world: ${new_project_.calculate()}!');
+}
---
+++ test/new_project__test.dart
@@ -0,0 +1,8 @@
+import 'package:new_project_/new_project_.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

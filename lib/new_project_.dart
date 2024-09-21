dfd44404e18d9f5cc52d2f7af0e28a97acd5a97d
Arsen <azybaliev91@gmail.com>
2024-09-21 20:20:50 +0300

    disarium call

--- lib/new_project_.dart
+++ lib/new_project_.dart
@@ -1,3 +1,17 @@
+import 'dart:math';
+
 int calculate() {
   return 6 * 7;
+}
+
+ String disarium(int n) {
+  int length = n.toString().length;
+  int sum = 0;
+  int nn = n;
+  for (var i = 1; i <= length; i++) {
+    int expon = (pow(10, length - i)).toInt();
+    sum += pow(nn ~/ expon, i).toInt();
+    nn %= expon;
+  }
+  return n == sum ? 'yes' : 'no';
 }

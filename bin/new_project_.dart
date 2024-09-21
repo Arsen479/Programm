3f1712e35ff358a52fcbf65b351de5f07b98c604
Arsen <azybaliev91@gmail.com>
2024-09-21 20:19:56 +0300

    disarium

--- bin/new_project_.dart
+++ bin/new_project_.dart
@@ -1,5 +1,17 @@
+import 'dart:math';
+
 import 'package:new_project_/new_project_.dart' as new_project_;
+import 'package:new_project_/new_project_.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${new_project_.calculate()}!');
+ 
+  print(new_project_.disarium(123));
+ 
+
+  //int length = len.length;
+  
+  //print(length);
+  //print('$a $b $c');
+  //print(exponention(b, length));
+
 }
